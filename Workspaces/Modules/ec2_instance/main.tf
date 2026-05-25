8f14ce700e12a9065d08a1dbdb002d3fb69f02ad
Yashvishnoi <jivishnoi@gmail.com>
2026-05-25 15:45:14 +0530

    Added the Dev folder and inside that added 2 workspace stage and prod

---
+++ Workspaces/Modules/ec2_instance/main.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+    region = "us-east-1"
+}
+variable "ami" {
+    description = "This is AMI for the Instance"
+}
+
+variable "instance_type" {
+    description = "This is instance type for the instance"
+}
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
+}
